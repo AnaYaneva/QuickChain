07508bfa53d57f6d4d2056548ec4301af8ede437
Martin Penkov <martin@penkov.me>
2018-02-16 21:37:23 +0200

    Initial Commit

---
+++ QuickChain/QuickChain.Miner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuickChain.Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
