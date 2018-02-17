6bfb476d09d64db7cc4d136f7c114a30a67a5b08
Antonio <antoniobuyukliev@yahoo.com>
2018-02-17 13:21:36 +0200

    added block and transactions models and blocks page

---
+++ QuickChain/QuickChain.Model/Block.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace QuickChain.Model
+{
+    public class Block
+    {
+        public int Height { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public IList<Transaction> Transactions { get; set; }
+        public string Hash { get; set; }
+        public string ParentHash { get; set; }
+        public BigInteger Difficulty { get; set; }
+        public int Size { get; set; }
+    }
+}
---
+++ QuickChain/QuickChain.Model/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickChain.Model
+{
+    public class Transaction
+    {
+        public string TxHash { get; set; }
+        public int BlockHeight { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public Decimal Value { get; set; }
+        public int UsedGas { get; set; }
+        public decimal Fee { get; set; }
+
+    }
+}
