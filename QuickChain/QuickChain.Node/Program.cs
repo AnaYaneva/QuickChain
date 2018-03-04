c8f0f40364b2723cc37754f8e46c010eebb5b10a
Martin Penkov <martin@penkov.me>
2018-03-04 12:50:50 +0200

    removed applications insights

--- QuickChain/QuickChain.Node/Program.cs
+++ QuickChain/QuickChain.Node/Program.cs
@@ -17,7 +17,6 @@ namespace QuickChain.Node
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
