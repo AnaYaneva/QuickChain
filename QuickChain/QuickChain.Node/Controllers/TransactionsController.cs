1faa916a61b9d9bf3e691169d526ed698bae0a70
Martin Penkov <martin@penkov.me>
2018-03-05 15:43:19 +0200

    implemented get all transactions in the node

--- QuickChain/QuickChain.Node/Controllers/TransactionsController.cs
+++ QuickChain/QuickChain.Node/Controllers/TransactionsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuickChain.Data;
 using QuickChain.Model;
 using QuickChain.Node.Model;
 using System;
@@ -11,32 +12,59 @@ namespace QuickChain.Node.Controllers
     [Route("[controller]")]
     public class TransactionsController : Controller
     {
-        [HttpGet]
-        public IEnumerable<Transaction> GetAll(string address = null, bool onlyConfirmed = false, bool onlyPending = false)
+        private readonly IRepository<Transaction> transactionsRepository;
+
+        public TransactionsController(IRepository<Transaction> transactionsRepository)
         {
-            return new Transaction[] { };
+            this.transactionsRepository = transactionsRepository;
         }
 
-        [HttpGet("{id}")]
-        public Transaction Get(string id)
+        [HttpGet]
+        public IEnumerable<Transaction> GetAll(bool? onlyConfirmed, bool? onlyPending, int? page, int? perPage, string address = null)
         {
-            return new Transaction();
+            IQueryable<Transaction> allTransactions = transactionsRepository.GetAll(false);
+
+            if (address != null)
+            {
+                allTransactions = allTransactions.Where(t => t.From == address || t.To == address);
+            }
+            if(onlyConfirmed == true)
+            {
+                allTransactions = allTransactions.Where(t => t.BlockHeight >= 0);
+            }
+            if(onlyPending == true)
+            {
+                allTransactions = allTransactions.Where(t => t.BlockHeight < 0);
+            }
+
+            return allTransactions.Skip(((page ?? 1) - 1) * (perPage ?? 20))
+                .Take(perPage ?? 20);
+        }
+
+        [HttpGet("{hash}")]
+        public Transaction Get(string hash)
+        {
+            return this.transactionsRepository.GetAll().Single(t => t.TxHash == hash);
         }
 
         [HttpPost()]
         public Transaction Create([FromBody]Transaction transaction)
         {
-            return transaction;
+            Transaction dbTransaction = this.transactionsRepository.Insert(transaction);
+            this.transactionsRepository.Save();
+
+            return dbTransaction;
+
         }
 
-        [HttpPost("{id}/sign")]
-        public Transaction Sign(string id, [FromBody]Signature signature)
+        [HttpPost("{hash}/sign")]
+        public Transaction Sign(string hash, [FromBody]Signature signature)
         {
             return new Transaction();
         }
 
-        [HttpPost("{id}/send")]
-        public Transaction Send(string id)
+        [HttpPost("{hash}/send")]
+        public Transaction Send(string hash)
         {
             return new Transaction();
         }
