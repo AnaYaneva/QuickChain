807d81531cf7e186c47f81442d9e70ae9d7497b5
JeanPetrov <narumcyt@gmail.com>
2018-03-07 00:13:37 +0200

    Add Miner PostRequestToNode

--- QuickChain/QuickChain.Miner/Infrastructure/MinerToNode.cs
+++ QuickChain/QuickChain.Miner/Infrastructure/MinerToNode.cs
@@ -1,8 +1,10 @@
 ﻿namespace QuickChain.Miner.Infrastructure
 {
+    using Newtonsoft.Json.Linq;
     using System;
     using System.IO;
     using System.Net;
+    using System.Text;
 
     public static class MinerToNode
     {
@@ -49,6 +51,58 @@
             response.Close();
 
             return responseFromNode;
+        }
+
+        public static void PostRequestToNode(string blockHash, string timestamp, long nonce)
+        {
+            JObject obj = JObject.FromObject(new
+            {
+                nonce = nonce.ToString(),
+                dateCreated = timestamp,
+                blockHash = blockHash
+            });
+
+            byte[] blockFoundData = Encoding.UTF8.GetBytes(obj.ToString());
+            int retries = 0;
+
+            HttpStatusCode statusCode = HttpStatusCode.OK;
+
+            do
+            {
+                try
+                {
+                    statusCode = HttpStatusCode.RequestTimeout;
+
+                                                        //TODO nodeIP   And                          minerAddres
+                    WebRequest request = WebRequest.Create("nodeIpAddress" + "/mining/get-block/" + "minerAddress");
+                    request.Method = "POST";
+                    request.Timeout = 3000;
+                    request.ContentType = "application/json; charset=utf-8";
+
+                    var dataStream = request.GetRequestStream();
+                    dataStream.Write(blockFoundData, 0, blockFoundData.Length);
+                    dataStream.Close();
+
+                    WebResponse response = request.GetResponse();
+                    statusCode = ((HttpWebResponse)response).StatusCode;
+
+                    Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+                    response.Close();
+                }
+                catch (WebException e)
+                {
+                    Console.WriteLine("WebException raised!");
+                    Console.WriteLine("{0}\n", e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception raised!");
+                    Console.WriteLine("Source : {0}", e.Source);
+                    Console.WriteLine("Message : {0}\n", e.Message);
+                }
+
+                System.Threading.Thread.Sleep(1000);
+            } while (statusCode != HttpStatusCode.OK && retries++ < 3);
         }
     }
 }
